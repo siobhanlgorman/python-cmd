55cd1ebba010a116af2fa4b86cacafccb5a11077
siobhanlgorman <siobhanlgorman@gmail.com>
2022-02-23 12:45:49 +0000

    Add file

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+# import module
 import os
-import subprocess
+
+# asssign directory
+directory1 = "mynewfolder"
+directory2 = "mynewfolder2"
+
+# create a dir
+def createdir():
+    os.system("mkdir mydir")
 
 
-def create():
-    os.system("mkdir mynewfolder")
+def runthroughtestfiles():
+    for filename in os.listdir(directory1):
+        if filename.is_file():
+            # f = os.path.join(directory, filename)
+            # # checking if it is a file
+            # if os.path.isfile(f):
+            print(filename)
 
 
-create()
+# def addfiletodir():
+#     createdir()
+#     os.system()
